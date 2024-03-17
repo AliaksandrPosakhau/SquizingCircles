7e66c46c599039944b71a51e14e339eb23100a82
Aliaksandr Posakhau <alyaksandr.posakhau@gmail.com>
2024-03-17 16:26:38 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SFML.Audio;
+using SFML.System;
+
+namespace SquizzingCircles
+{
+    internal class Program
+    {
+        static int FIRST_CIRCLE_INITIAL_RADIUS = 350;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started.");
+        }
+    }
+}
